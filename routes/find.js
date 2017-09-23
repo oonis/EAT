bc457ea5864289bdc8eb5826eaf8ed42f4b658d9
william <william.norman.dev@gmail.com>
2017-09-23 17:38:51 -0400

    Okay now just find msu

--- routes/find.js
+++ routes/find.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const router = express.Router()
 
+const msu-parse = require('../parsing/msu').process
+
 console.log('\t\tStarting route: /')
 
 router.get('/:category', function (req, res, next) {
-  res.render('find', {
-    user: req.user,
-    category: req.params.category
+
+  msu.parse(function(err, result) {
+    if (err) { throw err; }
+    res.render('find', {
+      user: req.user,
+      category: req.params.category,
+      results: results
+    })
   })
 })
 
