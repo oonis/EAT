2f773f32a8be36b0e5913d699e7f305d617b534a
william <lordcrekit@gmail.com>
2017-09-23 19:54:24 -0400

    Got rid of WINDOWS LINE ENDINGS

--- models/item.js
+++ models/item.js
@@ -1,14 +1,14 @@
 class Item {
-    constructor() {
-        this.name = "";
-        this.time = "";
-        this.options = [];
-    }
+  constructor () {
+    this.name = ''
+    this.time = ''
+    this.options = []
+  }
 
-    isVegan() {
-        return this.options.includes("vegan");
-    }
-    isVegetarian() {
-        return this.options.includes("vegetarian");
-    }
+  isVegan () {
+    return this.options.includes('vegan')
+  }
+  isVegetarian () {
+    return this.options.includes('vegetarian')
+  }
 }
