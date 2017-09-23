b9f5a1719f88ef77eed4c866bf2bbb46a55a3d8e
= <=>
2017-09-23 19:28:53 -0400

    working on making more objects

---
+++ models/item.js
@@ -0,0 +1,14 @@
+class Item {
+    constructor() {
+        this.name = "";
+        this.time = "";
+        this.options = [];
+    }
+
+    isVegan() {
+        return this.options.includes("vegan");
+    }
+    isVegetarian() {
+        return this.options.includes("vegetarian");
+    }
+}
