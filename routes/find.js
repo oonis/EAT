71906ffd4adebf66b6ef0cb5879ad355859e94aa
william <william.norman.dev@gmail.com>
2017-09-23 19:34:12 -0400

    Basic find page upgraded

--- routes/find.js
+++ routes/find.js
@@ -6,14 +6,47 @@ const msuParse = require('../parsing/msu').parse
 console.log('\t\tStarting route: /')
 
 router.get('/:category', function (req, res, next) {
-  msuParse(function (err, results) {
-    if (err) { throw err }
+  let category = req.params.category
+  if (category.toLowerCase() === 'msu') {
+    msuParse(function (err, results) {
+      if (err) { throw err }
+      res.render('find', {
+        user: req.user,
+        category: category,
+        results: results
+      })
+    })
+  } else if (category.toLowerCase() === 'uom') {
+    const results = {
+      Breakfast: {
+        Shaw: {
+          'Brocoli': [ 'vegan', 'vegetarian' ],
+          'Beef Tips': [ 'beef' ]
+        }
+      },
+      Lunch: {
+        Shaw: {
+          'lol': [ 'vegetarian' ]
+        }
+      },
+      Dinner: {
+        Shaw: {
+        }
+      },
+      Latenight: {
+        Shaw: {
+        }
+      }
+    }
+
     res.render('find', {
       user: req.user,
-      category: req.params.category,
+      category: category,
       results: results
     })
-  })
+  } else {
+    next()
+  }
 })
 
 module.exports = router
