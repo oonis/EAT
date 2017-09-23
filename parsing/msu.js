c9253615cf0133f5723a61e78776a67ad826632d
= <=>
2017-09-23 15:52:48 -0400

    fghvcbn

--- parsing/msu.js
+++ parsing/msu.js
@@ -1,7 +1,8 @@
 var request = require('request')
 var cheerio = require('cheerio')
+const Scraper = require ('./scraper')
 
-const process = function () {
+const process = function (cb) {
   var postURL = 'https://eatatstate.msu.edu'
 
   request({
@@ -12,16 +13,28 @@ const process = function () {
   }, function (error, response, body) {
     if (error) { throw error }
     var $ = cheerio.load(body)
-
+    var allHalls = []
     $('.dining-menu-name').each(function (i, elem) {
       var currentHallUrl = $(this).find('a').attr('href')
       if (currentHallUrl.indexOf('/menu/') !== 0) {
         // MSU makes it so if it isn't a hall it doesn't have /menu/
         return true // Not a hall so we don't care
       }
-      // var hallURL = postURL + currentHallUrl
+      var hallURL = postURL + currentHallUrl
+      allHalls.push(hallURL);
+
     })
+    var scrape = new Scraper(allHalls);
+    scrape.scrapeSites().then(console.log).catch(
+      function(err) {
+        throw err
+      }
+    )
+    
   })
+}
+function processHall(url) {
+
 }
 
 module.exports = {
