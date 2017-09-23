e1f1d48cfcf585fce00263467474f2bc14b33e1a
= <=>
2017-09-23 00:52:42 -0400

    getting the web scraping going!

--- parsing/generic.js
+++ parsing/generic.js
@@ -1,12 +1,20 @@
-const processPage = function(postURL, func) {
-    try {
-        jsdom.env({
-            done: func,
-            url: postURL
-        });
-    } catch(jsDomError) {
-        throw jsDomError;
-    }
+var request = require("request");
+var cheerio = require("cheerio");
+const processPage = function(postURL) {
+    request({
+        uri: postURL,
+        agentOptions: {
+            rejectUnauthorized: false
+        }
+      }, function(error, response, body) {
+            var $ = cheerio.load(body);
+            var vals = $('.meal-title').map(function(i, el) {
+                // this === el
+                return $(this).text()+"\n";
+            }).get().join(' ');
+            return;
+      });
+
 }
 
 module.exports = {
