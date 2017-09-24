a019ec0d6ad022582dd6dbedde8b3c467c5f76c4
william <lordcrekit@gmail.com>
2017-09-24 09:37:39 -0400

    working sorting

---
+++ parsing/uom.js
@@ -0,0 +1,32 @@
+const process = function (cb) {
+
+  const results = {
+    Breakfast: [
+    {
+      name: 'Shaw',
+      menu: [
+        { name: 'Beef Tips', tags: [ 'beef' ] },
+        { name: 'Brocoli', tags: [ 'vegan', 'vegetarian' ] },
+      ]
+    },
+    {
+      name: ':O',
+      menu: [
+        { name: 'Onion Rings', tags: [ 'vegan' ] }
+      ]
+    }
+    ],
+    Lunch: [
+    ],
+    Dinner: [
+    ],
+    Latenight: [
+    ]
+  }
+
+  cb(null, results)
+}
+
+module.exports = {
+  parse: process
+}
--- routes/find.js
+++ routes/find.js
@@ -1,51 +1,37 @@
 const express = require('express')
 const router = express.Router()
 
-const msuParse = require('../parsing/msu').parse
 const sort = require('../parsing/sort').sort
 
 console.log('\t\tStarting route: /')
 
+
+
+const parsers = {
+  msu: require('../parsing/msu').parse,
+  uom: require('../parsing/uom').parse
+}
+
 router.get('/:category', function (req, res, next) {
   let category = req.params.category
-  if (category.toLowerCase() === 'msu') {
-    msuParse(function (err, results) {
+  let key = category.toLowerCase()
+
+  let parser = parsers[key]
+  if (parser) {
+
+    parser(function (err, results) {
       if (err) { throw err }
-      sort(results, null)
+
+      // Blindly sort if they are logged in
+      if (req.user) { sort(results, null) }
+
       res.render('find', {
         user: req.user,
         category: category,
         results: results
       })
     })
-  } else if (category.toLowerCase() === 'uom') {
-    const results = {
-      Breakfast: {
-        Shaw: {
-          'Brocoli': [ 'vegan', 'vegetarian' ],
-          'Beef Tips': [ 'beef' ]
-        }
-      },
-      Lunch: {
-        Shaw: {
-          'lol': [ 'vegetarian' ]
-        }
-      },
-      Dinner: {
-        Shaw: {
-        }
-      },
-      Latenight: {
-        Shaw: {
-        }
-      }
-    }
 
-    res.render('find', {
-      user: req.user,
-      category: category,
-      results: results
-    })
   } else {
     next()
   }
